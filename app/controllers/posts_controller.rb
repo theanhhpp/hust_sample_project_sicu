27ec4ac50fa4adb22cfb54b305659683e731b49f
VuThiHien94 <khoanglang2368@gmail.com>
2016-03-19 17:46:48 +0700

    Post image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,20 @@
 class PostsController < ApplicationController
+	#before_action :authenticate_user!
+	 
 	before_action :set_post, only: [:show, :edit, :update, :destroy]
+	before_action :owned_post, only: [:edit, :update, :destroy]
 	def index 
 		 @posts = Post.all 
 	end
 	def new  
-  		@post = Post.new
+  		@post = current_user.posts.build
 	end
 	def create  
- 		if @post = Post.create(post_params)
+ 		@post = current_user.posts.build(post_params)
+	    if @post.save
 	      redirect_to posts_path
 	    else
+	      flash[:alert] = "Your new post couldn't be created!  Please check the form."
 	      render :new
 	    end
 	end
@@ -38,7 +43,14 @@ class PostsController < ApplicationController
 	    params.require(:post).permit(:image, :caption)
 	  end
 
-	  def set_post
-	    @post = Post.find(params[:id])
+	def set_post
+	  @post = Post.find(params[:id])
+	end
+
+	def owned_post  
+	  unless current_user == @post.user
+	    flash[:alert] = "That post doesn't belong to you!"
+	    redirect_to root_path
 	  end
+	end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319060227) do
+ActiveRecord::Schema.define(version: 20160319093212) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "caption"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20160319060227) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -37,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160319060227) do
     t.string   "name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "user_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
