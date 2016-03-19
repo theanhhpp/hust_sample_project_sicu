760077cc857c0b283afa8b479e4270dc84e538b3
VuThiHien94 <khoanglang2368@gmail.com>
2016-03-19 23:13:09 +0700

    add comment

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users, :controllers => { registrations: 'registrations' }  
-  resources :posts
+  resources :posts do  
+    resources :comments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
